631c21e1a1ce4bc511810bcb739756fa4ea3680e
Anoob <anup37@icloud.com>
2025-08-18 19:10:30 +0545

    Update FrameRate.h

--- WaveSurvivalGame/FrameRate.h
+++ WaveSurvivalGame/FrameRate.h
@@ -7,13 +7,13 @@ public:
     ~FrameRate();
 
     void Initialize();
-    void Load(); // Standardized to capital L
-    void Update(float deltaTime); // Changed to float deltaTime
+    void Load(); 
+    void Update(float deltaTime); 
     void Draw(sf::RenderWindow& window);
 
 private:
     sf::Text text;
     sf::Font font;
-    float timer; // Changed to float
-    int frameCount; // Added for more accurate FPS calculation
+    float timer; 
+    int frameCount; 
 };

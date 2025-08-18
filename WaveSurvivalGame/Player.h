02e288519391268109129915a0c8e98bb4dd2d08
Anoob <anup37@icloud.com>
2025-08-18 19:19:48 +0545

    Update Player.h

--- WaveSurvivalGame/Player.h
+++ WaveSurvivalGame/Player.h
@@ -6,7 +6,7 @@
 #include "Collision.h"
 #include "Sound.h"
 
-class Enemy; 
+class Enemy;
 
 class Player
 {
@@ -15,15 +15,15 @@ public:
     ~Player();
 
     void Load();
-    void Update(float deltaTime, std::vector<Enemy>& enemies, sf::RenderWindow& window, const std::vector<int>& tiles,float speed);
+    void Update(float deltaTime, std::vector<Enemy>& enemies, sf::RenderWindow& window, const std::vector<int>& tiles, float speed);
     void Draw(float deltaTime, sf::RenderWindow& window);
     void ChangeHealth(int hp);
 
-    sf::Sprite psprite; 
-    sf::FloatRect getGlobalBounds() const { return psprite.getGlobalBounds(); } 
+    sf::Sprite psprite;
+    sf::FloatRect getGlobalBounds() const { return psprite.getGlobalBounds(); }
 
-    sf::RectangleShape boundingRectangle; 
-    int getHealth() const { return health; } 
+    sf::RectangleShape boundingRectangle;
+    int getHealth() const { return health; }
 
 
     Sound* soundSystem;
@@ -32,25 +32,25 @@ private:
     sf::Texture idleTexture;
     sf::Texture attackTexture;
     sf::Texture walkTexture;
-    sf::Texture boneTexture; 
+    sf::Texture boneTexture;
     std::vector<sf::RectangleShape> boneBounds;
-    sf::Vector2i size; 
+    sf::Vector2i size;
 
     float boneSpeed;
     float boneFireRate;
-    sf::Clock boneFireClock; 
+    sf::Clock boneFireClock;
 
-    std::vector<sf::Sprite> bones; 
-    std::vector<sf::Vector2f> boneDirections; 
+    std::vector<sf::Sprite> bones;
+    std::vector<sf::Vector2f> boneDirections;
 
     float animationTimer = 0.0f;
     int currentFrame = 0;
-    float frameTime = 0.1f; 
-    int totalFrames = 6;    
+    float frameTime = 0.1f;
+    int totalFrames = 6;
 
-    int health; 
-    sf::Text healthText; 
-    sf::Font font; 
+    int health;
+    sf::Text healthText;
+    sf::Font font;
     Collision collision;
-    bool wasMoving = false; 
+    bool wasMoving = false;
 };

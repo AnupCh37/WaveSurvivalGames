9f9274024df60edc03edcd418a2c60f6e086a5e1
Anoob <anup37@icloud.com>
2025-08-18 19:17:48 +0545

    Update Math.h

--- WaveSurvivalGame/Math.h
+++ WaveSurvivalGame/Math.h
@@ -1,8 +1,8 @@
 #pragma once
-#include <SFML/Graphics.hpp> // Required for sf::Vector2f and sf::FloatRect
+#include <SFML/Graphics.hpp> 
 
 namespace Math {
     sf::Vector2f NormalizeVector(sf::Vector2f vector);
     bool DidRectCollision(const sf::FloatRect& rect1, const sf::FloatRect& rect2);
-    sf::Vector2f GetRectCenter(const sf::FloatRect& rect); // Declaration
+    sf::Vector2f GetRectCenter(const sf::FloatRect& rect); 
 }

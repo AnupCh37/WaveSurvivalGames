ccf0c60b203d886e2b4da8bdbd651d51108987c9
Anoob <anup37@icloud.com>
2025-08-18 19:18:37 +0545

    Update Menu.h

--- WaveSurvivalGame/Menu.h
+++ WaveSurvivalGame/Menu.h
@@ -22,36 +22,35 @@ public:
     void setGameOverStats(int survivedTime, int wavesSurvived, int enemiesKilled);
 
 private:
-    // Fonts and textures
+  
     sf::Font font;
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
 
-    // Intro Menu
+ 
     sf::Text titleText;
     sf::Text playButtonText;
     sf::Text exitButtonText;
     sf::RectangleShape playButton;
     sf::RectangleShape exitButton;
 
-    // Game Over Menu
+    
     sf::Text gameOverText;
     sf::Text statsText;
     sf::Text mainMenuButtonText;
     sf::RectangleShape mainMenuButton;
 
-    // Button states
+  
     bool playButtonHovered;
     bool exitButtonHovered;
     bool mainMenuButtonHovered;
     bool mousePressed;
 
-    // Game stats
     int survivalTime;
     int wavesSurvived;
     int enemiesKilled;
 
-    // Helper methods
+   
     void setupIntroMenu();
     void setupGameOverMenu();
     void updateIntroMenu(sf::RenderWindow& window, MenuState& gameState);
